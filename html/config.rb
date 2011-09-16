e0989f456c61fc1bfb1f60cdd1b8506919944009
Tedi Konda <tedi@tkdesigns.tv>
2011-09-16 00:35:39 -0400

    added page sections and started styling the header so it stands out

--- html/config.rb
+++ html/config.rb
@@ -1,5 +1,5 @@
-require 'sassy-buttons'
 # Require any additional compass plugins here.
+require 'sassy-buttons'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
@@ -7,4 +7,6 @@ sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
+relative_assets = true       
+output_style =  "expanded"
+line_comments = false
